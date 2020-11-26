60ba649b826f891b1ceac9ce02843674dccec9e6
Mihaela Cocieru <70962062+mihaelac01@users.noreply.github.com>
2020-11-26 11:52:16 +0300

    Add files via upload

---
+++ problema 1.py
@@ -0,0 +1,6 @@
+"""De la tastatura se citeste un sir de caractere.Elaborati un program care va determina:
+A)numarul de majuscule din sir;
+B)numarul de cifre din sir;
+C)numarul de caractere speciale(paranteze,operatori aritmetici) din sir;"""
+a=str(input("cum te cheama?"))
+print('salut',a)
